75e0a004009e9698fcb1f20aa26940cb58e7194c
Chris jurrens <crjurrens@gmail.com>
2019-02-18 22:45:31 -0600

    changed

--- Assn2/Destination.java
+++ Assn2/Destination.java
@@ -1,3 +1,96 @@
-public class Destination
+public class Destination implements Comparable<Destination>
 {
+  private String destination;
+  private int normalMiles;
+  private int frequentMiles;
+  private int firstClassMiles;
+  private int startMonth;
+  private int endMonth;
+  
+  public Destination ()
+  {
+    this.destination = "";
+    this.normalMiles = 0;
+    this.frequentMiles = 0;
+    this.firstClassMiles = 0;
+    this.startMonth = 0;
+    this.endMonth = 0;
+  }
+  
+  //Overloaded constructor.
+  public Destination (String destination, int normalMiles, int frequentMiles, int firstClassMiles, int startMonth, int endMonth)
+  {
+    this.destination = destination;
+    this.normalMiles = normalMiles;
+    this.frequentMiles = frequentMiles;
+    this.firstClassMiles = firstClassMiles;
+    this.startMonth = startMonth;
+    this.endMonth = endMonth;
+  }
+  
+  public String getDestination()
+  {
+    return destination;
+  }
+  
+  public void setDestination(String destination)
+  {
+    this.destination = destination;
+  }
+  
+  public int getNormalMiles()
+  {
+    return normalMiles;
+  }
+  
+  public void setNormalMiles(int normalMiles)
+  {
+    this.normalMiles = normalMiles;
+  }
+  
+  public int getFrequentMiles()
+  {
+    return frequentMiles;
+  }
+  
+  public void setFrequentMiles(int frequentMiles)
+  {
+    this.frequentMiles = frequentMiles;
+  }
+  
+  public int getFirstClassMiles()
+  {
+    return firstClassMiles;
+  }
+  
+  public void setFirstClassMiles(int firstClassMiles)
+  {
+    this.firstClassMiles = firstClassMiles;
+  }
+  
+  public int getStartMonth()
+  {
+    return startMonth;
+  }
+  
+  public void setStartMonth(int startMonth)
+  {
+    this.startMonth = startMonth;
+  }
+  
+  public int getEndMonth()
+  {
+    return endMonth;
+  }
+  
+  public void setEndMonth(int endMonth)
+  {
+    this.endMonth = endMonth;
+  }
+  
+  public int compareTo(Destination anotherDest)
+  {
+    return this.normalMiles - anotherDest.normalMiles;
+  }
+  
 }
--- Assn2/MileRedeemer.java
+++ Assn2/MileRedeemer.java
@@ -1,3 +1,124 @@
+import java.io.File; 
+import java.util.Scanner; 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MileRedeemer
 {
+  private ArrayList<Destination> flights;
+  private int remainingMiles;
+  
+  public MileRedeemer()
+  {
+    this.flights = new ArrayList<Destination>();
+    this.remainingMiles = 0;
+  }
+  
+  
+  public Destination[] getFlights ();
+  {
+    return flights;
+  }
+  
+  public void setFlights(Destination[] flights)
+  {
+    this.flights = flights; 
+  }
+  
+  public int getRemainingMiles()
+  {
+    return remainingMiles;
+  }
+  
+  public void setRemainingMiles(int remainingMiles)
+  {
+    this.remainingMiles = remainingMiles;
+  }
+ 
+  
+  public void readDestinations(Scanner fileScanner)
+  {   
+    Destination tempDest = new Destination();
+    
+    while (fileScanner.hasNext())
+    {
+      fileScanner.useDelimiter(";");
+      
+      tempDest.setDestination(fileScanner.next());
+      tempDest.setNormalMiles(Integer.parseInt(fileScanner.next()));
+      tempDest.setFrequentMiles(Integer.parseInt(fileScanner.next()));
+      tempDest.setFirstClassMiles(Integer.parseInt(fileScanner.next()));
+      
+      fileScanner.useDelimiter("-");
+      
+      tempDest.setStartMonth(Integer.parseInt(fileScanner.next()));
+      tempDest.setEndMonth(Integer.parseInt(fileScanner.next()));
+      
+      flights.add(tempDest);
+    }
+    
+    Collections.sort(flights);
+  }  
+  
+  public ArrayList<String> getCityNames()
+  {
+    int i = 0;
+    ArrayList<String> cityNames = new ArrayList<String>();
+    
+    while (i < flights.size())
+    {
+      cityNames.add(flights.get(i).getDestination());
+      i++;
+    }
+    
+    Collections.sort(cityNames);
+    
+    return cityNames;
+  }
+  
+  public ArrayList<String> redeemMiles(int miles, int month)
+  {
+    this.setRemainingMiles(miles); //Sets our remaining miles
+    ArrayList<String> cities = new ArrayList<String>; //This will hold all cities found in a descending order
+    while(cities.size() < 10) cities.add(""); //Set the size of the cities array to 10
+    String theCity = findFurthestCity(miles, cities); //This will hold city farthest away.
+     
+    
+    while (theCity != "" && int i < flights.size())
+    {
+      cities.add(theCity);
+      
+      compRemainingMiles(theCity, month, this.getRemainingMiles());
+      
+      
+  }
+  
+  public String findFurthestCity(int miles, ArrayList<String> cities)
+  { 
+    for (int i = 0; i < flights.size(); i++)
+    {
+      if (flights.get(i).getNormalMiles <= miles && !flights.get(i).getDestination.equals(cities.get(i)))
+        return flights.get(i).getDestination;
+    }
+    
+    return "";
+  }
+  
+  public void compRemainingMiles(String cityName, int month)
+  {
+    for (int i = 0; i < flights.size(); i++)
+    {
+      if (flights.get(i).getDestination.equals(cityName))
+      {
+        if (flights.get(i).getStartMonth() <= month && flights.get(i).getEndMonth() >= month)
+        {
+          this.remainingMiles  -= flights.get(i).getFrequentMiles();
+        }
+        else
+        {
+          this.remainMiles -= flights.get(i).getNormalMiles();
+        }
+      }
+    }
+  }
 }
